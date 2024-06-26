c501043397f889e1eba86a64850a15745f4033e6
LuisYepes601 <yepesluis006@gmail.com>
2024-06-25 19:01:08 -0500

    Nuevo cambio realizado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-console.log("Hola mundo")
+//console.log("Hola mundo")
+
+/**
+ * Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza 
+ * console.log para mostrar el mensaje "¡Hola, [tu nombre]!" en la consola del navegador.
+ */
+var nombre = "Luis";
+console.log(`Hola, tu nombre es: ${nombre}`);
